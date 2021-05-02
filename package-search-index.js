5b204e8569088434c781ee138da45ff579ba0d4f
BlackBaroness <shoyurash2@gmail.com>
2021-05-02 21:45:57 +0300

    massive refactor

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.baronessdev.paid.auth.api"},{"l":"ru.baronessdev.paid.auth.api.events"},{"l":"ru.baronessdev.paid.auth.api.gui"},{"l":"ru.baronessdev.paid.auth.api.subcommands"}]
